2a64ed3a9ed5fa4c412a24510177076f9f1e0ce1
RohanRameshRaut <rohanraut918@gmail.com>
2024-04-13 23:24:28 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s1;
+    cout<<"Enter a string: ";
+    getline(cin, s1); //
+    cout<<"string is"<<endl;
+    cout<<s1;
+    return 0;
+}
